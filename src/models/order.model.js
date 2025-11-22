baafca920e412d6aae67f1c68f49f72e23f8e7ee
lovepreetlavi97 <lovepreetlavi697@gmail.com>
2025-11-22 22:28:58 +0530

    app.js

--- src/models/order.model.js
+++ src/models/order.model.js
@@ -278,7 +278,7 @@ const orderSchema = new mongoose.Schema({
   statusHistory: [{
     status: {
       type: String,
-      enum: ['Pending', 'Processing', 'Shipped', 'Out for Delivery', 'Delivered', 'Cancelled', 'Returned', 'Refunded'],
+      enum: ['Pending', 'Processing',"Confirmed", 'Shipped', 'Out for Delivery', 'Delivered', 'Cancelled', 'Returned', 'Refunded'],
     },
     timestamp: {
       type: Date,
